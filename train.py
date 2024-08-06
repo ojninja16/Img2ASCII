d56f1aab6a644a0757f0a0a06eae37d3dcd70c1e
Ojas <dubeyojas16@gmail.com>
2024-08-06 15:42:14 +0530

    Trained a NN to replicate sin wave

--- train.py
+++ train.py
@@ -1,25 +1,34 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import matplotlib.pyplot as plt
 # import numpy as np
 
 class Network(nn.Module):
     def __init__(self):
         super().__init__() # here we don't need to pass the input and output size as we are not using any linear layer
-        self.layer = nn.Linear(1,1)# this is the default linear layer which takes input size as 1 and output size as 1
+        self.linear1=nn.Linear(1,5)
+        self.relu=nn.ReLU()
+        self.linear2=nn.Linear(5,5)
+        self.linear3=nn.Linear(5,1)# this is the default linear layer which takes input size as 1 and output size as 1
     def forward(self,x):
-        return self.layer(x)
+        x=self.linear1(x)
+        x=self.relu(x)  
+        x=self.linear2(x)
+        x=self.relu(x)
+        x=self.linear3(x)
+        return x
 def main():
     net=Network()
     # print=("par parameter",list(net.parameters()))
-    optimizer=optim.SGD(net.parameters(),lr=0.00001)# here we are passing the parameters of the network to the optimizer
+    optimizer=optim.SGD(net.parameters(),lr=0.01)# here we are passing the parameters of the network to the optimizer
     # print(net(torch.tensor([2.0])))
     i=0
     # print(list(net.named_parameters()))
     while True:
         optimizer.zero_grad()
-        inputs=torch.rand((8192,1))*1000-500
-        expected=inputs*4+3
+        inputs=torch.rand((8192,1))*2*3.1459
+        expected=torch.sin(inputs)
         # print(inputs.shape)
         ouput=net(inputs)
         loss=nn.MSELoss()(ouput,expected)
@@ -27,11 +36,19 @@ def main():
         optimizer.step()
         i+=1
         if i%1000==0:
-            print(loss)   
-        if loss <0.1:
+            print(f"Iteration {i}, Loss: {loss.item()}")
+        if loss <0.003:
             break
     net.eval()
-    print(net(torch.tensor([1.0,2.0,3.0,4.0,5.0]).reshape(-1,1)))     
-    print(list(net.parameters()))
+    test_inputs = torch.linspace(0, 2 * 3.1416, 100).reshape(-1, 1)
+    with torch.no_grad():
+        predictions = net(test_inputs)
+
+    # Plotting
+    plt.figure(figsize=(10, 5))
+    plt.plot(test_inputs.numpy(), predictions.numpy(), label='Predicted')
+    plt.plot(test_inputs.numpy(), torch.sin(test_inputs).numpy(), label='Actual Sine')
+    plt.legend()
+    plt.show()
 if __name__ == "__main__": 
     main()
